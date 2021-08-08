442c5ea34da0fbe7179cb1c990ae60e45b597866
chaoren-zg <88616225+chaoren-zg@users.noreply.github.com>
2021-08-08 22:27:37 +0800

    pull commit

--- src/main/java/com/zhouge/GitTest.java
+++ src/main/java/com/zhouge/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest
         System.out.println("master test!");
         System.out.println("hello git5!");
         System.out.println("hello git6!");
+        System.out.println("hello git7!");
     }
 }
